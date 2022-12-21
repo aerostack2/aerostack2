b7a8b6a00f6fddcb001823f810d891c1ac9bdc70
Miguel <miferco97@gmail.com>
2022-12-21 10:41:06 +0100

    state changed into unordered map to handle multiple SDKs

--- as2_aerial_platforms/as2_tello_platform/launch/tello_platform.launch.py
+++ as2_aerial_platforms/as2_tello_platform/launch/tello_platform.launch.py
@@ -10,7 +10,7 @@ import os
 
 def generate_launch_description():
     config = join(
-        get_package_share_directory('tello_platform'),
+        get_package_share_directory('as2_tello_platform'),
         'config',
         'control_modes.yaml'
     )
@@ -19,51 +19,28 @@ def generate_launch_description():
         DeclareLaunchArgument('drone_id',
                               default_value=DRONE_ID,
                               description='Drone namespace.'),
-        DeclareLaunchArgument('mass', default_value='0.029'),
-        DeclareLaunchArgument('max_thrust', default_value='0.0'),
-        DeclareLaunchArgument('min_thrust', default_value='0.0'),
         DeclareLaunchArgument('cmd_freq', default_value='10.0'),
         DeclareLaunchArgument('info_freq', default_value='10.0'),
         DeclareLaunchArgument('sensor_freq', default_value='10.0'),
         DeclareLaunchArgument('control_modes_file', default_value=config),
-        # DeclareLaunchArgument('drone_URI',
-        #                       default_value='radio://0/80/2M/E7E7E7E7E7',
-        #                       description='Crazyflie URI.'),
-        DeclareLaunchArgument('external_odom', default_value='false',
-                              choices=["true", "false"],
-                              description='Availability of external odometry'),
-        DeclareLaunchArgument('external_odom_topic',
-                              default_value='external_odom',
-                              description='External odometry topic name.'),
         DeclareLaunchArgument('simulation_mode', default_value='false',
                               choices=["true", "false"],
                               description="Simulation flag."),
-        # DeclareLaunchArgument('ip', default_value="192.168.43.95"),
-        # DeclareLaunchArgument('port', default_value='5000'),
-        # DeclareLaunchArgument('save_flag', default_value='False'),
-        # DeclareLaunchArgument('show_flag', default_value='False'),
 
         Node(
-            package="tello_platform",
-            executable="tello_platform_node",
+            package="as2_tello_platform",
+            executable="as2_tello_platform_node",
             name="platform",
             namespace=LaunchConfiguration('drone_id'),
             output="screen",
             emulate_tty=True,
             parameters=[{
                 "simulation_mode": LaunchConfiguration('simulation_mode'),
-                "mass": LaunchConfiguration('mass'),
-                "max_thrust": LaunchConfiguration('max_thrust'),
-                "min_thrust": LaunchConfiguration('min_thrust'),
                 "cmd_freq": LaunchConfiguration('cmd_freq'),
                 "info_freq": LaunchConfiguration('info_freq'),
                 "sensor_freq": LaunchConfiguration('sensor_freq'),
                 "control_modes_file":
                     LaunchConfiguration('control_modes_file'),
-                "external_odom": LaunchConfiguration('external_odom'),
-                # "drone_URI": LaunchConfiguration('drone_URI'),
-                "external_odom_topic":
-                    LaunchConfiguration('external_odom_topic'),
             }],
         )
     ])
