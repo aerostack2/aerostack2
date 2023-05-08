6a8056310f8e829e8b1d2f638aee2dfc00221c13
RPS98 <rafa.perez.s98@gmail.com>
2023-05-08 13:50:11 +0200

    Unify as2_behaviors_motion with config files

--- as2_behaviors/as2_behaviors_motion/launch/motion_behaviors_launch.py
+++ as2_behaviors/as2_behaviors_motion/launch/motion_behaviors_launch.py
@@ -1,104 +1,97 @@
-""" Launch the AS2 platform behaviors. """
-from launch import LaunchDescription
+""" Launch file for the motion behavior """
+
+import os
+import sys
+import logging
+from xml.etree import ElementTree
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch_ros.substitutions import FindPackageShare
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+
+FORMAT = '[%(levelname)s] [launch]: %(message)s'
+logging.basicConfig(format=FORMAT)
+
+
+def get_available_plugins(package_name: str, plugin_type: str) -> list[str]:
+    """
+    Parse plugins.xml file from package and return a list of plugins from a specific type
+    """
+    plugins_file = os.path.join(
+        get_package_share_directory(package_name),
+        'plugins.xml'
+    )
+    root = ElementTree.parse(plugins_file).getroot()
+
+    available_plugins = []
+    for class_element in root.findall('class'):
+        if plugin_type in class_element.attrib['type']:
+            available_plugins.append(
+                class_element.attrib['type'].split('::')[0])
+    return available_plugins
+
+
+def get_node(context, behavior_name: str):
+    """ Returns the behavior node """
+    plugin_name = LaunchConfiguration(
+        behavior_name + '_plugin_name').perform(context)
+    if not plugin_name:
+        logging.critical("Plugin not set.")
+        sys.exit(1)
+
+    parameters = [
+        {"use_sim_time": LaunchConfiguration('use_sim_time')},
+        {'plugin_name': plugin_name}
+    ]
+
+    behavior_config_file = LaunchConfiguration(
+        behavior_name + '_config_file').perform(context)
+
+    if not behavior_config_file:
+        behavior_config_file = PathJoinSubstitution([
+            FindPackageShare('as2_behaviors_motion'),
+            'config/' + behavior_name + '_behavior/config_default.yaml'
+        ])
+
+    parameters.append(behavior_config_file)
+
+    node = Node(
+        package='as2_behaviors_motion',
+        executable=behavior_name + '_behavior_node',
+        namespace=LaunchConfiguration('namespace'),
+        parameters=parameters,
+        output='screen',
+        emulate_tty=True
+    )
+
+    return [node]
 
 
 def generate_launch_description():
     """ Launch the AS2 platform behaviors. """
-    return LaunchDescription([
-        DeclareLaunchArgument('namespace'),
-        DeclareLaunchArgument('use_sim_time', default_value='false'),
 
-        DeclareLaunchArgument('follow_path_plugin_name'),
-        DeclareLaunchArgument('follow_path_speed', default_value="0.5"),
-        DeclareLaunchArgument('follow_path_threshold', default_value="0.3"),
-        Node(
-            package='as2_behaviors_motion',
-            executable='follow_path_behavior_node',
-            namespace=LaunchConfiguration('namespace'),
-            parameters=[
-                {"namespace": LaunchConfiguration('namespace')},
-                {"use_sim_time": LaunchConfiguration('use_sim_time')},
-                {"plugin_name": LaunchConfiguration(
-                    'follow_path_plugin_name')},
-                {"follow_path_speed": LaunchConfiguration(
-                    'follow_path_speed')},
-                {"follow_path_threshold": LaunchConfiguration(
-                    'follow_path_threshold')}
-            ],
-            output='screen',
-            emulate_tty=True
-        ),
+    behaviors = [
+        "follow_path",
+        "go_to",
+        "land",
+        "takeoff"]
 
-        DeclareLaunchArgument('go_to_plugin_name'),
-        DeclareLaunchArgument('go_to_speed', default_value="0.5"),
-        DeclareLaunchArgument('go_to_threshold', default_value="0.3"),
-        Node(
-            package='as2_behaviors_motion',
-            executable='go_to_behavior_node',
-            namespace=LaunchConfiguration('namespace'),
-            parameters=[
-                {"namespace": LaunchConfiguration('namespace')},
-                {"use_sim_time": LaunchConfiguration('use_sim_time')},
-                {"plugin_name": LaunchConfiguration('go_to_plugin_name')},
-                {"go_to_speed": LaunchConfiguration(
-                    'go_to_speed')},
-                {"go_to_threshold": LaunchConfiguration('go_to_threshold')}
-            ],
-            output='screen',
-            emulate_tty=True
-        ),
+    launch_description = []
 
-        DeclareLaunchArgument('land_plugin_name'),
-        DeclareLaunchArgument('land_speed', default_value="0.2"),
-        DeclareLaunchArgument(
-            'land_speed_condition_percentage', default_value="0.2"),
-        DeclareLaunchArgument(
-            'land_speed_condition_height', default_value="0.2"),
-        DeclareLaunchArgument(
-            'land_trajectory_height', default_value="-10.0"),
-        Node(
-            package='as2_behaviors_motion',
-            executable='land_behavior_node',
-            namespace=LaunchConfiguration('namespace'),
-            parameters=[
-                {"namespace": LaunchConfiguration('namespace')},
-                {"use_sim_time": LaunchConfiguration('use_sim_time')},
-                {"plugin_name": LaunchConfiguration('land_plugin_name')},
-                {"land_speed": LaunchConfiguration(
-                    'land_speed')},
-                {"land_speed_condition_percentage": LaunchConfiguration(
-                    'land_speed_condition_percentage')},
-                {"land_speed_condition_height": LaunchConfiguration(
-                    'land_speed_condition_height')},
-                {"land_trajectory_height": LaunchConfiguration(
-                    'land_trajectory_height')}
-            ],
-            output='screen',
-            emulate_tty=True
-        ),
+    launch_description.append(DeclareLaunchArgument('namespace'))
+    launch_description.append(DeclareLaunchArgument(
+        'use_sim_time', default_value='false'))
 
-        DeclareLaunchArgument('takeoff_plugin_name'),
-        DeclareLaunchArgument('takeoff_height', default_value="1.0"),
-        DeclareLaunchArgument('takeoff_speed', default_value="0.2"),
-        DeclareLaunchArgument('takeoff_threshold', default_value="0.1"),
-        Node(
-            package='as2_behaviors_motion',
-            executable='takeoff_behavior_node',
-            namespace=LaunchConfiguration('namespace'),
-            parameters=[
-                {"namespace": LaunchConfiguration('namespace')},
-                {"use_sim_time": LaunchConfiguration('use_sim_time')},
-                {"plugin_name": LaunchConfiguration('takeoff_plugin_name')},
-                {"takeoff_height": LaunchConfiguration(
-                    'takeoff_height')},
-                {"takeoff_speed": LaunchConfiguration(
-                    'takeoff_speed')},
-                {"takeoff_threshold": LaunchConfiguration('takeoff_threshold')}
-            ],
-            output='screen',
-            emulate_tty=True
-        )
-    ])
+    for behavior in behaviors:
+        launch_description.append(
+            DeclareLaunchArgument(behavior + '_plugin_name', description='Plugin name',
+                                  choices=get_available_plugins('as2_behaviors_motion', behavior)))
+        launch_description.append(
+            DeclareLaunchArgument(behavior + '_config_file', default_value='',
+                                  description='Path to behavior config file'))
+        launch_description.append(
+            OpaqueFunction(function=get_node, args=[behavior]))
+
+    return LaunchDescription(launch_description)
