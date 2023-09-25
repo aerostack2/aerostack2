319e9003a9183eaca333b14e0714b1f4dabe1c35
pariaspe <pedro.ariasp@upm.es>
2023-09-25 14:54:59 -0500

    some docker tools

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,6 +3,8 @@ set -e
 
 # setup ros2 environment
 source "/opt/ros/$ROS_DISTRO/setup.bash" --
-# source "${HOME}/aerostack2_ws/install/setup.bash"
-# source "${HOME}/aerostack2_ws/.env"
+source "${AEROSTACK2_WORKSPACE}/install/setup.bash"
+
+# TODO: source failing
+# source "$AEROSTACK2_PATH/as2_cli/setup_env.bash"
 exec "$@"
