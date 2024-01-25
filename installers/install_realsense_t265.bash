b7a68b10de7b6c35b889ede211f47fceaf1109c3
pariaspe <pedro.ariasp@upm.es>
2024-01-25 14:27:43 +0100

    remove unused files

--- installers/install_realsense_t265.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt install ros-${ROS_DISTRO}-realsense2-camera* &&
-sudo wget https://raw.githubusercontent.com/IntelRealSense/librealsense/master/config/99-realsense-libusb.rules --directory /etc/udev/rules.d/ 
-
-
-
-
