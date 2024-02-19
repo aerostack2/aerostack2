67842958af3e0d8260ac6679f03567fc1b5b10b5
pariaspe <pedro.ariasp@upm.es>
2024-02-19 15:57:46 +0100

    tests fix

--- as2_behaviors/as2_behaviors_perception/point_gimbal_behavior/include/point_gimbal_behavior/point_gimbal_behavior.hpp
+++ as2_behaviors/as2_behaviors_perception/point_gimbal_behavior/include/point_gimbal_behavior/point_gimbal_behavior.hpp
@@ -1,38 +1,44 @@
+// Copyright 2024 Universidad Politécnica de Madrid
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright
+//      notice, this list of conditions and the following disclaimer.
+//
+//    * Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in the
+//      documentation and/or other materials provided with the distribution.
+//
+//    * Neither the name of the Universidad Politécnica de Madrid nor the names of its
+//      contributors may be used to endorse or promote products derived from
+//      this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
 /*!*******************************************************************************************
  *  \file       point_gimbal_behavior.hpp
  *  \brief      Point Gimbal behavior header file.
  *  \authors    Pedro Arias-Perez
  *  \copyright  Copyright (c) 2024 Universidad Politécnica de Madrid
  *              All Rights Reserved
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- * 3. Neither the name of the copyright holder nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
- * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  ********************************************************************************/
 
-#ifndef POINT_GIMBAL_BEHAVIOR_HPP_
-#define POINT_GIMBAL_BEHAVIOR_HPP_
+#ifndef POINT_GIMBAL_BEHAVIOR__POINT_GIMBAL_BEHAVIOR_HPP_
+#define POINT_GIMBAL_BEHAVIOR__POINT_GIMBAL_BEHAVIOR_HPP_
 
+#include <string>
+#include <memory>
 #include <rclcpp/rclcpp.hpp>
 #include "as2_behavior/behavior_server.hpp"
 #include "as2_core/node.hpp"
@@ -44,34 +50,36 @@
 #include "geometry_msgs/msg/vector3.hpp"
 #include "geometry_msgs/msg/vector3_stamped.hpp"
 
-struct gimbal_status {
+struct gimbal_status
+{
   geometry_msgs::msg::Vector3 twist;
   geometry_msgs::msg::Vector3 orientation;
 };
 
-class PointGimbalBehavior : public as2_behavior::BehaviorServer<as2_msgs::action::FollowReference> {
+class PointGimbalBehavior : public as2_behavior::BehaviorServer<as2_msgs::action::FollowReference>
+{
 public:
   PointGimbalBehavior();
 
-  ~PointGimbalBehavior(){};
+  ~PointGimbalBehavior() {}
 
 private:
   bool on_activate(std::shared_ptr<const as2_msgs::action::FollowReference::Goal> goal) override;
 
   bool on_modify(std::shared_ptr<const as2_msgs::action::FollowReference::Goal> goal) override;
 
-  bool on_deactivate(const std::shared_ptr<std::string> &message) override;
+  bool on_deactivate(const std::shared_ptr<std::string> & message) override;
 
-  bool on_pause(const std::shared_ptr<std::string> &message) override;
+  bool on_pause(const std::shared_ptr<std::string> & message) override;
 
-  bool on_resume(const std::shared_ptr<std::string> &message) override;
+  bool on_resume(const std::shared_ptr<std::string> & message) override;
 
   as2_behavior::ExecutionStatus on_run(
-      const std::shared_ptr<const as2_msgs::action::FollowReference::Goal> &goal,
-      std::shared_ptr<as2_msgs::action::FollowReference::Feedback> &feedback_msg,
-      std::shared_ptr<as2_msgs::action::FollowReference::Result> &result_msg) override;
+    const std::shared_ptr<const as2_msgs::action::FollowReference::Goal> & goal,
+    std::shared_ptr<as2_msgs::action::FollowReference::Feedback> & feedback_msg,
+    std::shared_ptr<as2_msgs::action::FollowReference::Result> & result_msg) override;
 
-  void on_execution_end(const as2_behavior::ExecutionStatus &state) override;
+  void on_execution_end(const as2_behavior::ExecutionStatus & state) override;
 
 private:
   std::string gimbal_name_;
@@ -88,8 +96,9 @@ private:
   void gimbal_twist_callback(const geometry_msgs::msg::Vector3Stamped::SharedPtr msg);
 
   // AUX
-  bool compareAttitude(const geometry_msgs::msg::Point &attitude1,
-                       const geometry_msgs::msg::Vector3 &attitude2);
+  bool compareAttitude(
+    const geometry_msgs::msg::Point & attitude1,
+    const geometry_msgs::msg::Vector3 & attitude2);
 };
 
-#endif  // POINT_GIMBAL_BEHAVIOR_HPP_
+#endif  // POINT_GIMBAL_BEHAVIOR__POINT_GIMBAL_BEHAVIOR_HPP_
--- as2_behaviors/as2_behaviors_perception/point_gimbal_behavior/launch/point_gimbal_behavior.launch.py
+++ as2_behaviors/as2_behaviors_perception/point_gimbal_behavior/launch/point_gimbal_behavior.launch.py
@@ -1,22 +1,44 @@
-""" Launch file for point gimbal behavior node. """
+# Copyright 2024 Universidad Politécnica de Madrid
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#
+#    * Neither the name of the the copyright holder nor the names of its
+#      contributors may be used to endorse or promote products derived from
+#      this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
 
-import os
+"""Launch file for point gimbal behavior node."""
+
 from launch_ros.actions import Node
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration, EnvironmentVariable
-from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
-    """ Launch point gimbal behavior node. """
-
-    # config = os.path.join(get_package_share_directory('as2_behaviors_perception'),
-    #                       'point_gimbal_behavior/config/params.yaml')
-
+    """Launch point gimbal behavior node."""
     return LaunchDescription([
-        DeclareLaunchArgument('namespace', description='Drone namespace', default_value=EnvironmentVariable(
-            'AEROSTACK2_SIMULATION_DRONE_ID')),
+        DeclareLaunchArgument('namespace', description='Drone namespace',
+                              default_value=EnvironmentVariable('AEROSTACK2_SIMULATION_DRONE_ID')),
         DeclareLaunchArgument('use_sim_time', default_value='false'),
         DeclareLaunchArgument('log_level', default_value='info'),
         DeclareLaunchArgument('gimbal_name', description='Name of the gimbal'),
