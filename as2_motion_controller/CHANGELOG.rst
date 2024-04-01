1623628dbc796ed0c23276c366611b9e9a2e6cef
Rafael Perez-Segui <rafa.perez.s98@gmail.com>
2024-04-01 14:41:32 +0200

    Use ament_lint_auto

--- as2_motion_controller/CHANGELOG.rst
+++ as2_motion_controller/CHANGELOG.rst
@@ -33,7 +33,8 @@ Changelog for package as2_motion_controller
 
 1.0.2 (2023-08-17)
 ------------------
-* Merge pull request `#275 <https://github.com/aerostack2/aerostack2/issues/275>`_ from aerostack2/274-as2_motion_controller-parameter-changes-are-not-applied-properly
+* Merge pull request `#275 <https://github.com/aerostack2/aerostack2/issues/275>`_ from
+  aerostack2/274-as2_motion_controller-parameter-changes-are-not-applied-properly
   [as2_motion_controller] Change updateParams input from std::string to rclcpp::Parameter
 * Change updateParams input from std::string to rclcpp::Parameter
 * Convert motion controller params to config file
@@ -41,15 +42,18 @@ Changelog for package as2_motion_controller
 
 1.0.1 (2023-04-25)
 ------------------
-* Merge pull request `#227 <https://github.com/aerostack2/aerostack2/issues/227>`_ from aerostack2/226-as2_motion_controller-failed-to-load-differential_flatness_controller
+* Merge pull request `#227 <https://github.com/aerostack2/aerostack2/issues/227>`_ from
+  aerostack2/226-as2_motion_controller-failed-to-load-differential_flatness_controller
   [as2_motion_controller] Fix failed to load differential_flatness_controller
 * Namespace and Plugin Name must be equals
 * Merge pull request `#223 <https://github.com/aerostack2/aerostack2/issues/223>`_ from aerostack2/200-unify-maintainer-in-packagexmls
   Maintainer unified to CVAR-UPM
 * Maintainer unified to CVAR-UPM
-* Merge pull request `#210 <https://github.com/aerostack2/aerostack2/issues/210>`_ from aerostack2/207-as2_controller_manager-fill-info-message
+* Merge pull request `#210 <https://github.com/aerostack2/aerostack2/issues/210>`_ from
+  aerostack2/207-as2_controller_manager-fill-info-message
   [as2_controller_manager] Fill info message
-* Merge pull request `#212 <https://github.com/aerostack2/aerostack2/issues/212>`_ from aerostack2/211-nodes-should-use-aerostack2-node-create-timer-method-not-create-wall-timer
+* Merge pull request `#212 <https://github.com/aerostack2/aerostack2/issues/212>`_ from
+  aerostack2/211-nodes-should-use-aerostack2-node-create-timer-method-not-create-wall-timer
   Nodes use Aerostack2 Node Create Timer method, not create wall timer
 * Change create wall timer to as2 create timer
 * Fix freq parameters names
--- as2_motion_controller/include/as2_motion_controller/controller_base.hpp
+++ as2_motion_controller/include/as2_motion_controller/controller_base.hpp
@@ -1,76 +1,79 @@
-/********************************************************************************************
+// Copyright 2023 Universidad Politécnica de Madrid
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright
+//      notice, this list of conditions and the following disclaimer.
+//
+//    * Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in the
+//      documentation and/or other materials provided with the distribution.
+//
+//    * Neither the name of the Universidad Politécnica de Madrid nor the names of its
+//      contributors may be used to endorse or promote products derived from
+//      this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+/*!*******************************************************************************************
  *  \file       controller_base.hpp
  *  \brief      Declares the as2_motion_controller_plugin_base class which is the base
  *class for all controller plugins.
- *
  *  \authors    Miguel Fernández Cortizas
  *              Rafael Pérez Seguí
- *              Pedro Arias Pérez
- *              David Pérez Saura
- *
- *  \copyright  Copyright (c) 2022 Universidad Politécnica de Madrid
- *              All Rights Reserved
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- * 3. Neither the name of the copyright holder nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
- * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- ********************************************************************************/
+ ********************************************************************************************/
 
-#ifndef CONTROLLER_BASE_HPP
-#define CONTROLLER_BASE_HPP
+#ifndef AS2_MOTION_CONTROLLER__CONTROLLER_BASE_HPP_
+#define AS2_MOTION_CONTROLLER__CONTROLLER_BASE_HPP_
 
+#include <string>
+#include <vector>
+#include <rclcpp/rclcpp.hpp>
 #include <geometry_msgs/msg/pose_stamped.hpp>
 #include <geometry_msgs/msg/twist_stamped.hpp>
-#include <rclcpp/rclcpp.hpp>
 
 #include "as2_core/node.hpp"
 #include "as2_msgs/msg/control_mode.hpp"
 #include "as2_msgs/msg/thrust.hpp"
 #include "as2_msgs/msg/trajectory_point.hpp"
 
-namespace as2_motion_controller_plugin_base {
+namespace as2_motion_controller_plugin_base
+{
 
-class ControllerBase {
+class ControllerBase
+{
 public:
   /*
    * @brief Constructor
    */
-  ControllerBase(){};
+  ControllerBase() {}
 
   /*
    * @brief Initialize the controller plugin, since it is a plugin the Constructor must not have
    * parameters
    * @param node_ptr as2::Node pointer to be used by the controller plugin
    */
-  void initialize(as2::Node* node_ptr) {
+  void initialize(as2::Node * node_ptr)
+  {
     node_ptr_ = node_ptr;
     ownInitialize();
-  };
+  }
 
   /*
    * @brief Own initialize function to be implemented by the controller plugin
    */
-  virtual void ownInitialize(){};
+  virtual void ownInitialize() {}
 
   /*
    * @brief Update the State obtained from the sensors to be used by the controller plugin
@@ -79,28 +82,29 @@ public:
    * @param twist_msg geometry_msgs::msg::TwistStamped message with the current twist of the robot
    * in the "base_link" frame
    */
-  virtual void updateState(const geometry_msgs::msg::PoseStamped& pose_msg,
-                           const geometry_msgs::msg::TwistStamped& twist_msg) = 0;
+  virtual void updateState(
+    const geometry_msgs::msg::PoseStamped & pose_msg,
+    const geometry_msgs::msg::TwistStamped & twist_msg) = 0;
 
   /*
    * @brief Update the pose reference to be used by the controller plugin
    * @param ref geometry_msgs::msg::PoseStamped message with the current pose of the robot in
    * the "odom" frame
    */
-  virtual void updateReference(const geometry_msgs::msg::PoseStamped& ref){};
+  virtual void updateReference(const geometry_msgs::msg::PoseStamped & ref) {}
 
   /*
    * @brief Update the speedreference to be used by the controller plugin
    * @param ref geometry_msgs::msg::TwistStamped message with the current twist of the robot in the
    * "base_link" frame
    */
-  virtual void updateReference(const geometry_msgs::msg::TwistStamped& ref){};
+  virtual void updateReference(const geometry_msgs::msg::TwistStamped & ref) {}
   /*
    * @brief Update the reference to be used by the controller plugin
    * @param ref as2_msgs::msg::TrajectoryPoint message with the current reference of
    * the robot in the "odom" frame
    */
-  virtual void updateReference(const as2_msgs::msg::TrajectoryPoint& ref){};
+  virtual void updateReference(const as2_msgs::msg::TrajectoryPoint & ref) {}
 
   // virtual void updateReference(const as2_msgs::msg::Thrust& ref){};
 
@@ -112,18 +116,20 @@ public:
    * frame will depend on the output control mode
    * @param thrust as2_msgs::msg::Thrust message with the output thrust of the robot
    */
-  virtual bool computeOutput(double dt,
-                             geometry_msgs::msg::PoseStamped& pose,
-                             geometry_msgs::msg::TwistStamped& twist,
-                             as2_msgs::msg::Thrust& thrust) = 0;
+  virtual bool computeOutput(
+    double dt,
+    geometry_msgs::msg::PoseStamped & pose,
+    geometry_msgs::msg::TwistStamped & twist,
+    as2_msgs::msg::Thrust & thrust) = 0;
   /*
    * @brief Update the control mode to be used by the controller plugin
    * @param mode_in as2_msgs::msg::ControlMode message with the desired input control mode
    * @param mode_out as2_msgs::msg::ControlMode message with the desired output control mode
    * @return bool true if the in-out control mode configuration is valid, false otherwise
    */
-  virtual bool setMode(const as2_msgs::msg::ControlMode& mode_in,
-                       const as2_msgs::msg::ControlMode& mode_out) = 0;
+  virtual bool setMode(
+    const as2_msgs::msg::ControlMode & mode_in,
+    const as2_msgs::msg::ControlMode & mode_out) = 0;
 
   /*
    * @brief Update the parameters of the controller plugin
@@ -131,7 +137,7 @@ public:
    * Manager Node
    * @return bool true if the parameters are updated correctly, false otherwise
    */
-  virtual bool updateParams(const std::vector<rclcpp::Parameter>& _params_list) = 0;
+  virtual bool updateParams(const std::vector<rclcpp::Parameter> & _params_list) = 0;
 
   /*
    * @brief Reset the internal state of the controller plugin
@@ -142,26 +148,25 @@ public:
    * @brief Get the desired frame_id of the state and reference pose msgs
    * By default it is "odom"
    */
-  virtual std::string getDesiredPoseFrameId() { return "odom"; }
+  virtual std::string getDesiredPoseFrameId() {return "odom";}
 
   /*
    * @brief Get the desired frame_id of the state and reference twist msgs
    * By default it is "base_link"
    */
-  virtual std::string getDesiredTwistFrameId() { return "base_link"; }
+  virtual std::string getDesiredTwistFrameId() {return "base_link";}
 
   /*
    * @brief Destructor
    */
-  virtual ~ControllerBase(){};
+  virtual ~ControllerBase() {}
 
 protected:
   /* @brief as2::Node pointer to be used by the controller plugin */
-  inline as2::Node* getNodePtr() { return node_ptr_; }
-  as2::Node* node_ptr_;
+  inline as2::Node * getNodePtr() {return node_ptr_;}
+  as2::Node * node_ptr_;
+};   //  class ControllerBase
 
-};  //  ControllerBase
+}  // namespace as2_motion_controller_plugin_base
 
-};  // namespace as2_motion_controller_plugin_base
-
-#endif  // CONTROLLER_BASE_HPP
+#endif  // AS2_MOTION_CONTROLLER__CONTROLLER_BASE_HPP_
