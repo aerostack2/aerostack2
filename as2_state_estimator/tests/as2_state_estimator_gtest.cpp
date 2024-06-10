5cea858df3cc0047455c192c054f28b341f84db1
Rafael Perez-Segui <rafa.perez.s98@gmail.com>
2024-06-10 19:01:58 +0200

    Adding gtest for plugin loading

--- as2_state_estimator/tests/as2_state_estimator_gtest.cpp
+++ as2_state_estimator/tests/as2_state_estimator_gtest.cpp
@@ -27,84 +27,17 @@
 // POSSIBILITY OF SUCH DAMAGE.
 
 /**
-* @file mocap_gtest.hpp
+* @file as2_state_estimator_gtest.hpp
 *
-* An state estimation plugin mocap_pose for AeroStack2 gtest
+* An state estimation gtest
 *
 * @authors Rafael Pérez Seguí
-*          Javier Melero Deza
-*          Pedro Arias Pérez
 */
 
 #include <gtest/gtest.h>
-#include <random>
-#include <rclcpp/rclcpp.hpp>
-#include <rclcpp/executors/multi_threaded_executor.hpp>
 #include <ament_index_cpp/get_package_share_directory.hpp>
 
-#include <as2_core/names/topics.hpp>
-#include <as2_core/node.hpp>
-#include <as2_core/utils/frame_utils.hpp>
-#include <as2_core/utils/tf_utils.hpp>
-
 #include "as2_state_estimator.hpp"
-
-#define SPEED 0.50
-
-class MocapMock : public as2::Node
-{
-public:
-  MocapMock()
-  : as2::Node("mocap_pose_mock")
-  {
-    mocap_pose_pub_ = create_publisher<geometry_msgs::msg::PoseStamped>(
-      as2_names::topics::ground_truth::pose, as2_names::topics::ground_truth::qos);
-    timer_ =
-      create_timer(std::chrono::milliseconds(10), std::bind(&MocapMock::timer_callback, this));
-
-    msg.pose.position.x = 0.0;
-    msg.pose.position.y = 0.0;
-    msg.pose.position.z = 0.0;
-    as2::frame::eulerToQuaternion(0.0, 0.0, M_PI_2 / 2.0, msg.pose.orientation);
-    // as2::frame::eulerToQuaternion(0.0, 0.0, 0.0, msg.pose.orientation);
-  }
-
-private:
-  rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr mocap_pose_pub_;
-  rclcpp::TimerBase::SharedPtr timer_;
-  geometry_msgs::msg::PoseStamped msg;
-
-  double compute_dt()
-  {
-    static rclcpp::Time last_time = now();
-    rclcpp::Time current_time = now();
-    double dt = (current_time - last_time).seconds();
-    last_time = current_time;
-    return dt;
-  }
-
-  double generate_noise()
-  {
-    static std::default_random_engine generator;
-    static std::normal_distribution<double> distribution(0.0, 0.01);
-    return distribution(generator);
-  }
-
-  void timer_callback()
-  {
-    auto dt = compute_dt();
-    msg.header.stamp = now();
-    msg.header.frame_id = "earth";
-    msg.pose.position.x += SPEED * dt + generate_noise();
-    /* msg.pose.position.y += SPEED * dt + generate_noise();
-    msg.pose.position.z += SPEED * dt + generate_noise(); */
-    /* msg.pose.orientation.x = 0.0;
-    msg.pose.orientation.y = 0.0;
-    msg.pose.orientation.z = 0.0;
-    msg.pose.orientation.w = 1.0; */
-    mocap_pose_pub_->publish(msg);
-  }
-};
 
 std::shared_ptr<as2_state_estimator::StateEstimator> getStateEstimatorNode(
   const std::string plugin_name)
@@ -137,32 +70,40 @@ std::shared_ptr<as2_state_estimator::StateEstimator> getStateEstimatorNode(
   return std::make_shared<as2_state_estimator::StateEstimator>(node_options);
 }
 
-TEST(MocapMock, MocapMock) {
-  auto mocap_pose_node = getStateEstimatorNode("mocap_pose");
-  auto mock = std::make_shared<MocapMock>();
+TEST(As2StateEstimatorGTest, PluginLoadGroundTruth) {
+  EXPECT_NO_THROW(getStateEstimatorNode("ground_truth"));
+  auto node = getStateEstimatorNode("ground_truth");
 
-  std::shared_ptr<tf2_ros::Buffer> tf_buffer = std::make_shared<tf2_ros::Buffer>(
-    mocap_pose_node->get_clock());
-  std::shared_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster =
-    std::make_shared<tf2_ros::TransformBroadcaster>(mocap_pose_node);
-  std::shared_ptr<tf2_ros::StaticTransformBroadcaster> static_tf_broadcaster =
-    std::make_shared<tf2_ros::StaticTransformBroadcaster>(mocap_pose_node);
-
+  // Spin the node
   rclcpp::executors::MultiThreadedExecutor executor;
-  executor.add_node(mocap_pose_node);
-  executor.add_node(mock);
-  for (int i = 0; i < 100; i++) {
-    executor.spin_some();
-    std::this_thread::sleep_for(std::chrono::milliseconds(10));
-  }
-  rclcpp::shutdown();
-  ASSERT_TRUE([]() {return true;});
+  executor.add_node(node);
+  executor.spin_some();
+}
+
+TEST(As2StateEstimatorGTest, PluginLoadMocapPose) {
+  EXPECT_NO_THROW(getStateEstimatorNode("mocap_pose"));
+  auto node = getStateEstimatorNode("mocap_pose");
+
+  // Spin the node
+  rclcpp::executors::MultiThreadedExecutor executor;
+  executor.add_node(node);
+  executor.spin_some();
+}
+
+TEST(As2StateEstimatorGTest, PluginLoadRawOdometry) {
+  EXPECT_NO_THROW(getStateEstimatorNode("raw_odometry"));
+  auto node = getStateEstimatorNode("raw_odometry");
+
+  // Spin the node
+  rclcpp::executors::MultiThreadedExecutor executor;
+  executor.add_node(node);
+  executor.spin_some();
 }
 
 int main(int argc, char ** argv)
 {
-  rclcpp::init(argc, argv);
   ::testing::InitGoogleTest(&argc, argv);
+  rclcpp::init(argc, argv);
   auto result = RUN_ALL_TESTS();
   rclcpp::shutdown();
   return result;
