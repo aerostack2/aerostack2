bbf73d34cfbd40e662dfadfee438aabd29436e0d
pariaspe <pedro.ariasp@upm.es>
2024-06-19 13:08:43 +0200

    update state estimator launcher

--- as2_state_estimator/launch/state_estimator_launch.py
+++ as2_state_estimator/launch/state_estimator_launch.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright 2023 Universidad Politécnica de Madrid
+# Copyright 2024 Universidad Politécnica de Madrid
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
@@ -30,53 +30,50 @@
 
 """Launch file for the state estimator node."""
 
-__authors__ = 'Rafael Pérez Seguí'
-__copyright__ = 'Copyright (c) 2022 Universidad Politécnica de Madrid'
+__authors__ = 'Pedro Arias Pérez, Rafael Pérez Seguí'
+__copyright__ = 'Copyright (c) 2024 Universidad Politécnica de Madrid'
 __license__ = 'BSD-3-Clause'
-__version__ = '0.1.0'
 
 import os
 
 from ament_index_python.packages import get_package_share_directory
-import as2_core.launch_param_utils as as2_utils
 from as2_core.launch_plugin_utils import get_available_plugins
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, OpaqueFunction
-from launch.substitutions import EnvironmentVariable, LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import EnvironmentVariable, LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 
+from as2_core.declare_launch_arguments_from_config_file import DeclareLaunchArgumentsFromConfigFile
+from as2_core.launch_configuration_from_config_file import LaunchConfigurationFromConfigFile
 
-def get_package_config_file():
-    """Return the package config file."""
+
+def generate_launch_description() -> LaunchDescription:
+    """Entry point for launch file."""
     package_folder = get_package_share_directory('as2_state_estimator')
-    return os.path.join(package_folder,
-                        'config/state_estimator_default.yaml')
-
-
-def get_node(context, *args, **kwargs) -> list:
-    """
-    Get node.
-
-    :param context: Launch context
-    :type context: LaunchContext
-    :return: List with node
-    :rtype: list
-    """
-    # Get plugin name
-    plugin_name = LaunchConfiguration('plugin_name').perform(context)
-
-    # Get plugin configuration file
-    package_folder = get_package_share_directory(
-        'as2_state_estimator')
-    plugin_config_file = os.path.join(package_folder,
-                                      'plugins/' +
-                                      plugin_name +
-                                      '/config/plugin_default.yaml')
-
-    return [
-        *as2_utils.declare_launch_arguments(
-            'plugin_config_file',
-            default_value=plugin_config_file,
+    config_file = os.path.join(package_folder,
+                               'config/state_estimator_default.yaml')
+    plugin_config_file = PathJoinSubstitution([
+        package_folder,
+        'plugins', LaunchConfiguration('plugin_name'), 'config/plugin_default.yaml'
+    ])
+    return LaunchDescription([
+        DeclareLaunchArgument('log_level',
+                              description='Logging level',
+                              default_value='info'),
+        DeclareLaunchArgument('use_sim_time',
+                              description='Use simulation clock if true',
+                              default_value='false'),
+        DeclareLaunchArgument('namespace',
+                              description='Drone namespace',
+                              default_value=EnvironmentVariable('AEROSTACK2_SIMULATION_DRONE_ID')),
+        DeclareLaunchArgument('plugin_name',
+                              description='Plugin name',
+                              choices=get_available_plugins('as2_state_estimator')),
+        DeclareLaunchArgumentsFromConfigFile(
+            name='config_file', source_file=config_file,
+            description='Configuration file'),
+        DeclareLaunchArgumentsFromConfigFile(
+            name='plugin_config_file', source_file=plugin_config_file,
             description='Plugin configuration file'),
         Node(
             package='as2_state_estimator',
@@ -88,46 +85,16 @@ def get_node(context, *args, **kwargs) -> list:
                        LaunchConfiguration('log_level')],
             emulate_tty=True,
             parameters=[
-                *as2_utils.launch_configuration(
-                    'config_file',
-                    default_value=get_package_config_file()),
-                *as2_utils.launch_configuration(
-                    'plugin_config_file',
-                    default_value=plugin_config_file),
                 {
                     'use_sim_time': LaunchConfiguration('use_sim_time'),
-                    'plugin_name': plugin_name
+                    'plugin_name': LaunchConfiguration('plugin_name')
                 },
+                LaunchConfigurationFromConfigFile(
+                    'config_file',
+                    default_file=config_file),
+                LaunchConfigurationFromConfigFile(
+                    'plugin_config_file',
+                    default_file=plugin_config_file),
             ]
         )
-    ]
-
-
-def generate_launch_description() -> LaunchDescription:
-    """
-    Entry point for launch file.
-
-    :return: Launch description
-    :rtype: LaunchDescription
-    """
-    return LaunchDescription([
-        DeclareLaunchArgument('log_level',
-                              description='Logging level',
-                              default_value='info'),
-        DeclareLaunchArgument('use_sim_time',
-                              description='Use simulation clock if true',
-                              default_value='false'),
-        DeclareLaunchArgument('namespace',
-                              description='Drone namespace',
-                              default_value=EnvironmentVariable(
-                                  'AEROSTACK2_SIMULATION_DRONE_ID')),
-        DeclareLaunchArgument('plugin_name',
-                              description='Plugin name',
-                              choices=get_available_plugins(
-                                  'as2_state_estimator')),
-        *as2_utils.declare_launch_arguments(
-            'config_file',
-            default_value=get_package_config_file(),
-            description='Configuration file'),
-        OpaqueFunction(function=get_node)
     ])
