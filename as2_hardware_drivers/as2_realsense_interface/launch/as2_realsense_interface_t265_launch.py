54d4e905781640ea3716c318c48cc68c4356ce61
Javilinos <javier.mdeza@alumnos.upm.es>
2024-06-24 16:36:07 +0200

    realsense node passes clang tests

--- as2_hardware_drivers/as2_realsense_interface/launch/as2_realsense_interface_t265_launch.py
+++ as2_hardware_drivers/as2_realsense_interface/launch/as2_realsense_interface_t265_launch.py
@@ -1,10 +1,47 @@
+#!/usr/bin/env python3
+
+# Copyright 2024 Universidad Politécnica de Madrid
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#
+#    * Neither the name of the Universidad Politécnica de Madrid nor the names of its
+#      contributors may be used to endorse or promote products derived from
+#      this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+"""Launch file for the as2_realsense_t265_interface package."""
+
+__authors__ = 'David Pérez Saura'
+__copyright__ = 'Copyright (c) 2024 Universidad Politécnica de Madrid'
+__license__ = 'BSD-3-Clause'
+
 from launch import LaunchDescription
-from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument, OpaqueFunction 
+from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
 package_name = 'as2_realsense_interface'
+
 
 def generate_launch_description():
     # Declare arguments
@@ -15,7 +52,7 @@ def generate_launch_description():
     tf_device_config = PathJoinSubstitution([
         FindPackageShare(package_name),
         'config/tf_device',
-        device+'.yaml'
+        device + '.yaml'
     ])
 
     return LaunchDescription([
@@ -24,12 +61,12 @@ def generate_launch_description():
         DeclareLaunchArgument('tf_device_config', default_value=tf_device_config),
 
         Node(
-          package=package_name,
-          executable=package_name+'_node',
-          namespace=LaunchConfiguration('namespace'),
-          parameters=[{'node_frequency': LaunchConfiguration('node_frequency')},
-                      LaunchConfiguration('tf_device_config')],
-          output='screen',
-          emulate_tty=True,
+            package=package_name,
+            executable=package_name + '_node',
+            namespace=LaunchConfiguration('namespace'),
+            parameters=[{'node_frequency': LaunchConfiguration('node_frequency')},
+                        LaunchConfiguration('tf_device_config')],
+            output='screen',
+            emulate_tty=True,
         )
-  ])
+    ])
