6afae6888017524b2b8727b4049c7cef42cad186
Javilinos <javier.mdeza@alumnos.upm.es>
2024-06-25 17:42:14 +0200

    all tests and new format set, still gtest to pass due to device needs to be connected for the constructor to stay alive

--- as2_hardware_drivers/as2_usb_camera_interface/include/as2_usb_camera_interface/as2_usb_camera_interface.hpp
+++ as2_hardware_drivers/as2_usb_camera_interface/include/as2_usb_camera_interface/as2_usb_camera_interface.hpp
@@ -1,68 +1,80 @@
-/*!*******************************************************************************************
- *  \file       usb_camera_interface.hpp
- *  \brief      usb camera interface header file.
- *  \authors    David Perez Saura
- *              Miguel Fernandez Cortizas
- *  \copyright  Copyright (c) 2022 Universidad Politécnica de Madrid
- *              All Rights Reserved
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- * 3. Neither the name of the copyright holder nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
- * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- ********************************************************************************/
+// Copyright 2024 Universidad Politécnica de Madrid
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright
+//      notice, this list of conditions and the following disclaimer.
+//
+//    * Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in the
+//      documentation and/or other materials provided with the distribution.
+//
+//    * Neither the name of the Universidad Politécnica de Madrid nor the names of its
+//      contributors may be used to endorse or promote products derived from
+//      this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
 
-#ifndef USB_CAMERA_INTERFACE_HPP_
-#define USB_CAMERA_INTERFACE_HPP_
+/**
+* @file as2_usb_camera_interface.hpp
+*
+* @brief Class definition for the USB camera interface node
+*
+* @authors David Perez Saura, Miguel Fernandez Cortizas
+*/
+
+#ifndef AS2_USB_CAMERA_INTERFACE__AS2_USB_CAMERA_INTERFACE_HPP_
+#define AS2_USB_CAMERA_INTERFACE__AS2_USB_CAMERA_INTERFACE_HPP_
+
+#include <tf2/LinearMath/Quaternion.h>
+#include <cv_bridge/cv_bridge.h>
+
+#include <vector>
+#include <memory>
+#include <string>
 
 #include <rclcpp/rclcpp.hpp>
 #include "as2_core/names/topics.hpp"
 #include "as2_core/node.hpp"
 #include "as2_core/sensor.hpp"
 
-#include <tf2/LinearMath/Quaternion.h>
 #include "sensor_msgs/image_encodings.hpp"
 #include "sensor_msgs/msg/image.hpp"
 
-#include <cv_bridge/cv_bridge.h>
+
 #include <image_transport/image_transport.hpp>
-#include <memory>
 #include <opencv2/calib3d.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/opencv.hpp>
-#include <vector>
 
-class UsbCameraInterface : public as2::Node {
+
+namespace usb_camera_interface
+{
+
+class UsbCameraInterface : public as2::Node
+{
 public:
   /**
    * @brief Construct a new UsbCameraInterface object
    */
-  UsbCameraInterface();
+  explicit UsbCameraInterface(const rclcpp::NodeOptions & options = rclcpp::NodeOptions());
 
   /**
    * @brief Destroy the UsbCameraInterface object
    */
-  ~UsbCameraInterface(){};
+  ~UsbCameraInterface() {}
 
 private:
   std::shared_ptr<as2::sensors::Camera> camera_;
@@ -82,12 +94,14 @@ private:
 
   cv::VideoCapture cap_;
 
-  void setCameraParameters(const cv::Mat &_camera_matrix, const cv::Mat &_dist_coeffs);
+  void setCameraParameters(const cv::Mat & _camera_matrix, const cv::Mat & _dist_coeffs);
   void loadParameters();
   void captureImage();
   void setupCamera();
   void setCameraTransform();
-  void setCameraModelTransform(const std::string &_camera_flu, const std::string &_camera_rdf);
+  void setCameraModelTransform(const std::string & _camera_flu, const std::string & _camera_rdf);
 };
 
-#endif  // USB_CAMERA_INTERFACE_HPP_
+}  // namespace usb_camera_interface
+
+#endif  // AS2_USB_CAMERA_INTERFACE__AS2_USB_CAMERA_INTERFACE_HPP_
