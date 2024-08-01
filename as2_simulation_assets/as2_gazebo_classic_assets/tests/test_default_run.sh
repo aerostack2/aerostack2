5254e543e26a832d7a6a3ea37a24e8e318208fac
pariaspe <pedro.ariasp@upm.es>
2024-08-01 10:35:20 +0200

    as2_gazebo_classic EOL

--- as2_simulation_assets/as2_gazebo_classic_assets/tests/test_default_run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Changing PX4 GPS origin
-export PX4_HOME_LAT=28.143971
-export PX4_HOME_LON=-16.503213
-export PX4_HOME_ALT=0
-
-${AS2_GZ_ASSETS_SCRIPT_PATH}/default_run.sh "config.json"
--- as2_simulation_assets/as2_gazebo_classic_assets/tests/test_run_sitl.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-PX4_FOLDER="${AEROSTACK2_WORKSPACE}/src/thirdparty/PX4-Autopilot"
-(cd $PX4_FOLDER; DONT_RUN=1 make px4_sitl gazebo-classic)
-export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$PX4_FOLDER:$PX4_FOLDER/Tools/sitl_gazebo
-
-$AS2_GZ_ASSETS_SCRIPT_PATH/run_sitl.sh "$PX4_FOLDER/build/px4_sitl_default/bin/px4" "config.json" $PX4_FOLDER $PX4_FOLDER/build/px4_sitl_default
-
