0d972c82480c2ccc8ff36c22ae239265cf2c13c3
Rafael Perez-Segui <rafa.perez.s98@gmail.com>
2024-10-30 11:53:49 +0100

    Use as2::spingLoop without preset_loop_frequency

--- as2_hardware_drivers/as2_usb_camera_interface/src/as2_usb_camera_interface_node.cpp
+++ as2_hardware_drivers/as2_usb_camera_interface/src/as2_usb_camera_interface_node.cpp
@@ -41,9 +41,7 @@ int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
   auto node = std::make_shared<usb_camera_interface::UsbCameraInterface>();
-  node->preset_loop_frequency(30);
   as2::spinLoop(node);
-
   rclcpp::shutdown();
   return 0;
 }
