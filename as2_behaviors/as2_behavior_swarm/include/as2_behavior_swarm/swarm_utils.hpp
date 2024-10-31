dd4c95d8aa52a32853348ae58e05e028109a7f3f
Carmen De Rojas Pita-Romero <c.derojas@alumnos.upm.es>
2024-10-31 13:09:13 +0100

    Generation of Tf. Initializaion of drones with inicial pose. Drones follows theirs tf_ref

--- as2_behaviors/as2_behavior_swarm/include/as2_behavior_swarm/swarm_utils.hpp
+++ as2_behaviors/as2_behavior_swarm/include/as2_behavior_swarm/swarm_utils.hpp
@@ -26,54 +26,18 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
-#ifndef DRONE_SWARM_HPP_
-#define DRONE_SWARM_HPP_
+#ifndef SWARM_UTILS_HPP_
+#define SWARM_UTILS_HPP_
 
-#include <memory>
 #include <string>
+#include <utility>
+#include <memory>
 #include <vector>
-
 #include <rclcpp/rclcpp.hpp>
-
-#include "tf2_ros/transform_broadcaster.h"
-#include "tf2_ros/static_transform_broadcaster.h"
 #include "geometry_msgs/msg/pose_stamped.hpp"
-#include "as2_core/names/topics.hpp"
-#include "as2_core/utils/tf_utils.hpp"
-#include "as2_msgs/msg/platform_info.hpp"
-#include "as2_motion_reference_handlers/position_motion.hpp"
-#include "tf2_ros/transform_listener.h"
-#include "tf2_ros/buffer.h"
-#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
-#include <swarm_utils.hpp>
 
 
-class DroneSwarm
-{
-public:
-  DroneSwarm(as2::Node * node_ptr, std::string drone_id, geometry_msgs::msg::Pose init_pose);
-  ~DroneSwarm() {}
-
-private:
-  as2::Node * node_ptr_;
-  void drone_pose_callback(const geometry_msgs::msg::PoseStamped::SharedPtr _pose_msg);
-  void platform_info_callback(const as2_msgs::msg::PlatformInfo::SharedPtr _platform_info_msg);
-  std::shared_ptr<as2::tf::TfHandler> tf_handler_;
-  std::shared_ptr<tf2_ros::StaticTransformBroadcaster> tfstatic_broadcaster_;
-  std::string base_link_frame_id_;
-  rclcpp::Subscription<as2_msgs::msg::PlatformInfo>::SharedPtr platform_info_sub_;
-  rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr drone_pose_sub_;
-  std::shared_ptr<as2::motionReferenceHandlers::PositionMotion> position_motion_handler_ = nullptr;
-
-public:
-  std::string drone_id_;
-  geometry_msgs::msg::Pose init_pose_;
-  geometry_msgs::msg::PoseStamped drone_pose_;
-  geometry_msgs::msg::PoseStamped new_drone_pose_;
-  as2_msgs::msg::PlatformInfo platform_info_;
-  geometry_msgs::msg::TransformStamped transform;
-
-};
+std::vector<geometry_msgs::msg::Pose> two_drones(geometry_msgs::msg::PoseStamped centroid);
 
 
-#endif // DRONE_SWARM_HPP_
+#endif  // SWARM_UTILS_HPP_
