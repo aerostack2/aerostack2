39cd9da7f425d29ce0f18409a96aeaddcdcb641c
carmendrpr <c.derojas@alumnos.upm.es>
2024-11-12 18:45:54 +0100

    Drone's FollowReference working on frame drone_ref and Swarm's FollowPath working

--- as2_behaviors/as2_behavior_swarm/src/drone_swarm.cpp
+++ as2_behaviors/as2_behavior_swarm/src/drone_swarm.cpp
@@ -31,27 +31,24 @@
 
 DroneSwarm::DroneSwarm(
   as2::Node * node_ptr, std::string drone_id,
-  geometry_msgs::msg::Pose init_pose)
+  geometry_msgs::msg::Pose init_pose, rclcpp::CallbackGroup::SharedPtr cbk_group)
 : node_ptr_(node_ptr)
 {
-  tfstatic_broadcaster_ = std::make_shared<tf2_ros::StaticTransformBroadcaster>(node_ptr);
+  RCLCPP_INFO(node_ptr_->get_logger(), "Init %s", drone_id.c_str());
   drone_id_ = drone_id;
   init_pose_ = init_pose;
+  cbk_group_ = cbk_group;
+
   drone_pose_sub_ = node_ptr_->create_subscription<geometry_msgs::msg::PoseStamped>(
     drone_id_ + "/" + as2_names::topics::self_localization::pose,
     as2_names::topics::self_localization::qos,
     std::bind(&DroneSwarm::drone_pose_callback, this, std::placeholders::_1));
 
-  // TO DO
-  tf_handler_ = std::make_shared<as2::tf::TfHandler>(node_ptr_);
-
+  // Static tf (Swarm --> drone_ref)
   base_link_frame_id_ = as2::tf::generateTfName(
     node_ptr, drone_id_ + "_ref");
   parent_frame_id = as2::tf::generateTfName(node_ptr, "Swarm");
-  RCLCPP_INFO(node_ptr->get_logger(), "base_link_frame_id_: %s", base_link_frame_id_.c_str());
-
-
-  // Static tf
+  tfstatic_broadcaster_ = std::make_shared<tf2_ros::StaticTransformBroadcaster>(node_ptr);
   transform.header.stamp = node_ptr->get_clock()->now();
   transform.header.frame_id = parent_frame_id;
   transform.child_frame_id = base_link_frame_id_;
@@ -60,123 +57,46 @@ DroneSwarm::DroneSwarm(
   transform.transform.translation.z = init_pose.position.z;
   tfstatic_broadcaster_->sendTransform(transform);
 
-  // position_motion_handler_ = std::make_shared<as2::motionReferenceHandlers::PositionMotion>(
-  //   node_ptr_, drone_id_);
-  platform_info_sub_ = node_ptr->create_subscription<as2_msgs::msg::PlatformInfo>(
-    drone_id_ + "/" + as2_names::topics::platform::info, as2_names::topics::platform::qos,
-    std::bind(&DroneSwarm::platform_info_callback, this, std::placeholders::_1));
-  cbk_group_ = node_ptr->create_callback_group(
-    rclcpp::CallbackGroupType::MutuallyExclusive);
-
-  go_to_client_ = rclcpp_action::create_client<as2_msgs::action::GoToWaypoint>(
-    node_ptr_,
-    drone_id_ + "/" + as2_names::actions::behaviors::gotowaypoint, cbk_group_);
+  // FollowReference
   follow_reference_client_ = rclcpp_action::create_client<as2_msgs::action::FollowReference>(
     node_ptr_,
-    drone_id_ + "/" + as2_names::actions::behaviors::followreference, cbk_group_);
-
+    "/" + drone_id_ + "/" + as2_names::actions::behaviors::followreference, cbk_group_);
 }
-bool DroneSwarm::own_init()
+
+std::shared_ptr<rclcpp_action::ClientGoalHandle<as2_msgs::action::FollowReference>>
+DroneSwarm::own_init()
 {
-  // /* GoTO server */
-  // if (!go_to_client_->wait_for_action_server(std::chrono::seconds(5))) {
-  //   RCLCPP_ERROR(
-  //     node_ptr_->get_logger(),
-  //     "GoTo Action server not available "
-  //     "after waiting. Aborting.");
-  //   return false;
-  // }
-
-  // auto goal_msg = as2_msgs::action::GoToWaypoint::Goal();
-  // goal_msg.target_pose.header.frame_id = "earth";
-  // goal_msg.target_pose.header.stamp = node_ptr_->get_clock()->now();
-  // goal_msg.target_pose.point.x = init_pose_.position.x;
-  // goal_msg.target_pose.point.y = init_pose_.position.y;
-  // goal_msg.target_pose.point.z = init_pose_.position.z;
-  // goal_msg.yaw.mode = as2_msgs::msg::YawMode::KEEP_YAW;
-  // goal_msg.max_speed = 0.5;
-
-
-  // auto goal_handle_future_go_to = go_to_client_->async_send_goal(goal_msg);
-
-  // RCLCPP_INFO(node_ptr_->get_logger(), "Sending goal to GoTo behavior");
-  // auto goal_handle_go_to = goal_handle_future_go_to.get();
-  // if (goal_handle_go_to == nullptr) {
-  //   RCLCPP_ERROR(node_ptr_->get_logger(), "GoTo rejected from server");
-  //   return false;
-  // } else {
-  //   RCLCPP_INFO(node_ptr_->get_logger(), "GoTo accepted from server");
-  // }
-
-  /* FollowReference */
+  RCLCPP_INFO(node_ptr_->get_logger(), "Init %s FollowReference", drone_id_.c_str());
   if (!follow_reference_client_->wait_for_action_server(
       std::chrono::seconds(5)))
   {
     RCLCPP_ERROR(
       node_ptr_->get_logger(),
       "Follow Reference Action server not available after waiting.");
-    return false;
+    return nullptr;
   }
+
+  // Reference to follow
   auto goal_reference_msg = as2_msgs::action::FollowReference::Goal();
-  goal_reference_msg.target_pose.header.frame_id = "earth";
+  goal_reference_msg.target_pose.header.frame_id = "Swarm/" + drone_id_ + "_ref";
   goal_reference_msg.target_pose.header.stamp = node_ptr_->get_clock()->now();
-  goal_reference_msg.target_pose.point.x = init_pose_.position.x;
-  goal_reference_msg.target_pose.point.y = init_pose_.position.y;
-  goal_reference_msg.target_pose.point.z = init_pose_.position.z;
+  goal_reference_msg.target_pose.point.x = 0;
+  goal_reference_msg.target_pose.point.y = 0;
+  goal_reference_msg.target_pose.point.z = 0;
   goal_reference_msg.yaw.mode = as2_msgs::msg::YawMode::KEEP_YAW;
   goal_reference_msg.max_speed_x = 0.5;
   goal_reference_msg.max_speed_y = 0.5;
   goal_reference_msg.max_speed_z = 0.5;
 
+  // Handle future
   auto goal_handle_future_follow_reference = follow_reference_client_->async_send_goal(
     goal_reference_msg);
-  /*cuando haga el control, tengo que ver como le voy pasando la velocidad*/
-  RCLCPP_INFO(node_ptr_->get_logger(), "Sending reference to FollowReference behavior");
   auto goal_handle_follow_reference = goal_handle_future_follow_reference.get();
-  if (goal_handle_follow_reference == nullptr) {
-    RCLCPP_ERROR(node_ptr_->get_logger(), "FollowReference rejected from server");
-    return false;
-  } else {
-    RCLCPP_INFO(node_ptr_->get_logger(), "FollowReference accepted from server");
-  }
-  return true;
+  return goal_handle_follow_reference;
 }
 
 
 void DroneSwarm::drone_pose_callback(const geometry_msgs::msg::PoseStamped::SharedPtr _pose_msg)
 {
-  // self_localization is always on frame earth
   drone_pose_ = *_pose_msg;
-}
-
-// TO DO
-void DroneSwarm::platform_info_callback(
-  const as2_msgs::msg::PlatformInfo::SharedPtr _platform_info_msg)
-{
-  platform_info_ = *_platform_info_msg;
-}
-
-// Check status of FollowReference
-as2_behavior::ExecutionStatus DroneSwarm::on_run(
-  const rclcpp_action::ClientGoalHandle<as2_msgs::action::FollowReference>::SharedPtr & goal_handle)
-{
-  switch (goal_handle->get_status()) {
-    case rclcpp_action::GoalStatus::STATUS_EXECUTING:
-      RCLCPP_INFO(node_ptr_->get_logger(), "FollowReference is executing");
-      return as2_behavior::ExecutionStatus::RUNNING;
-    case rclcpp_action::GoalStatus::STATUS_SUCCEEDED:
-      RCLCPP_INFO(node_ptr_->get_logger(), "FollowReference succeded");
-      return as2_behavior::ExecutionStatus::SUCCESS;
-    case rclcpp_action::GoalStatus::STATUS_CANCELED:
-      RCLCPP_ERROR(node_ptr_->get_logger(), "FollowReference was canceled.");
-      return as2_behavior::ExecutionStatus::FAILURE;
-    case rclcpp_action::GoalStatus::STATUS_ABORTED:
-      RCLCPP_ERROR(node_ptr_->get_logger(), "FollowReference was aborted.");
-      return as2_behavior::ExecutionStatus::FAILURE;
-    default:
-      RCLCPP_INFO(node_ptr_->get_logger(), "FollowReference is executing");
-      return as2_behavior::ExecutionStatus::RUNNING;
-  }
-
-
 }
