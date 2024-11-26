f507b8c8e9906b4028b84d1af176fe9e23c3b656
keikei014 <fjac014@gmail.com>
2024-11-26 16:27:38 +0100

    ACRO commands publisher added.

--- as2_aerial_platforms/as2_platform_gazebo/include/as2_platform_gazebo/as2_platform_gazebo.hpp
+++ as2_aerial_platforms/as2_platform_gazebo/include/as2_platform_gazebo/as2_platform_gazebo.hpp
@@ -51,6 +51,9 @@
 #include "as2_core/utils/control_mode_utils.hpp"
 #include "as2_core/utils/tf_utils.hpp"
 
+#include "as2_msgs/msg/thrust.hpp"
+#include "as2_msgs/msg/acro.hpp"
+
 namespace gazebo_platform
 {
 
@@ -74,6 +77,7 @@ public:
   // Publishers
   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr twist_pub_;
   rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr arm_pub_;
+  rclcpp::Publisher<as2_msgs::msg::Acro>::SharedPtr acro_pub_;
 
   // Subscribers
   rclcpp::Subscription<geometry_msgs::msg::TwistStamped>::SharedPtr twist_state_sub_;
