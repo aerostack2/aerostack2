d4c58d52df97547518dd47e19716e4db7f6e9f4a
GPatiA2 <g.glenza@upm.es>
2025-04-23 08:08:01 +0000

    Fix copyright year

--- as2_user_interfaces/as2_visualization/as2_visualization/as2_visualization/drone_viz.py
+++ as2_user_interfaces/as2_visualization/as2_visualization/as2_visualization/drone_viz.py
@@ -1,4 +1,4 @@
-# Copyright 2024 Universidad Politécnica de Madrid
+# Copyright 2025 Universidad Politécnica de Madrid
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
