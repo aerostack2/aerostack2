c1a579fff8ac4995790c695c4047f2629dfbd6b1
Rafael Perez-Segui <rafa.perez.s98@gmail.com>
2026-08-19 12:39:18 +0200

    [as2_core] Vendor tf2_ros::TransformListener from ROS 2 jazzy
    Byte for byte copy of tf2_ros/transform_listener.hpp and tf2_ros/src/transform_listener.cpp
    from the jazzy branch of ros2/geometry2, so that the aerostack2 changes applied on top show
    up as a diff of their own.
    
    This commit alone does not build: the vendored file still declares
    tf2_ros::TransformListener while tf_utils already uses as2::tf::FilteredTransformListener.

--- as2_core/include/as2_core/utils/filtered_transform_listener.hpp
+++ as2_core/include/as2_core/utils/filtered_transform_listener.hpp
@@ -1,72 +1,87 @@
-// Copyright 2023 Universidad Politécnica de Madrid
-// Copyright (c) 2008, Willow Garage, Inc.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met:
-//
-//    * Redistributions of source code must retain the above copyright
-//      notice, this list of conditions and the following disclaimer.
-//
-//    * Redistributions in binary form must reproduce the above copyright
-//      notice, this list of conditions and the following disclaimer in the
-//      documentation and/or other materials provided with the distribution.
-//
-//    * Neither the name of the Universidad Politécnica de Madrid nor the names of its
-//      contributors may be used to endorse or promote products derived from
-//      this software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-// POSSIBILITY OF SUCH DAMAGE.
+/*
+ * Copyright (c) 2008, Willow Garage, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Willow Garage, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
-/*!*******************************************************************************************
- *  \file       filtered_transform_listener.hpp
- *  \brief      filtered_transform_listener header file.
- *  \authors    Tully Foote
- *              Miguel Fernandez Cortizas
- *              Rafael Perez Segui
- *              Rodrigo Da Silva Gómez
- ********************************************************************************/
+/** \file
+ *  \brief Author: Tully Foote
+ */
 
-
-#ifndef AS2_CORE__UTILS__FILTERED_TRANSFORM_LISTENER_HPP_
-#define AS2_CORE__UTILS__FILTERED_TRANSFORM_LISTENER_HPP_
+#ifndef TF2_ROS__TRANSFORM_LISTENER_HPP_
+#define TF2_ROS__TRANSFORM_LISTENER_HPP_
 
 #include <functional>
 #include <memory>
 #include <thread>
 #include <utility>
-#include <vector>
 
+#include "tf2/buffer_core.hpp"
+#include "tf2/time.hpp"
+#include "tf2_ros/visibility_control.hpp"
 
-#include "tf2/tf2/buffer_core.h"
-#include "tf2/tf2/time.h"
-#include "tf2_ros/visibility_control.h"
-#include "tf2_ros/transform_listener.h"
+#include "tf2_msgs/msg/tf_message.hpp"
+#include "rclcpp/rclcpp.hpp"
 
 #include "tf2_ros/qos.hpp"
 
-namespace as2
+namespace tf2_ros
 {
 
-namespace tf
+namespace detail
 {
+template<class AllocatorT = std::allocator<void>>
+rclcpp::SubscriptionOptionsWithAllocator<AllocatorT>
+get_default_transform_listener_sub_options()
+{
+  rclcpp::SubscriptionOptionsWithAllocator<AllocatorT> options;
+  options.qos_overriding_options = rclcpp::QosOverridingOptions{
+    rclcpp::QosPolicyKind::Depth,
+    rclcpp::QosPolicyKind::Durability,
+    rclcpp::QosPolicyKind::History,
+    rclcpp::QosPolicyKind::Reliability};
+  return options;
+}
 
-// /**
-//  * @brief This class is similar to the tf2_ros::TransformListener but it allows to filter the
-//  * transforms that are received by the listener and added to the buffer. We based on the code of
-//  * the tf2_ros::TransformListener and we added the filter functionality in the subscribe
-//  * function.
-//  */
-class FilteredTransformListener
+template<class AllocatorT = std::allocator<void>>
+rclcpp::SubscriptionOptionsWithAllocator<AllocatorT>
+get_default_transform_listener_static_sub_options()
+{
+  rclcpp::SubscriptionOptionsWithAllocator<AllocatorT> options;
+  options.qos_overriding_options = rclcpp::QosOverridingOptions{
+    rclcpp::QosPolicyKind::Depth,
+    rclcpp::QosPolicyKind::History,
+    rclcpp::QosPolicyKind::Reliability};
+  return options;
+}
+}  // namespace detail
+
+/** \brief This class provides an easy way to request and receive coordinate frame transform information.
+ */
+class TransformListener
 {
 public:
   /** \brief Simplified constructor for transform listener.
@@ -76,25 +91,57 @@ public:
    * to it, then it's recommended to use one of the other constructors.
    */
   TF2_ROS_PUBLIC
-  explicit FilteredTransformListener(
+  explicit TransformListener(tf2::BufferCore & buffer, bool spin_thread = true);
+
+  /** \brief Simplified constructor for transform listener with static_only option.
+   *
+   * This constructor will create a new ROS 2 node under the hood.
+   * If you already have access to a ROS 2 node and you want to associate the TransformListener
+   * to it, then it's recommended to use one of the other constructors.
+   */
+  TF2_ROS_PUBLIC
+  explicit TransformListener(
     tf2::BufferCore & buffer,
-    bool spin_thread = true,
-    bool static_only = false);
+    bool spin_thread,
+    bool static_only);
 
   /** \brief Node constructor */
   template<class NodeT, class AllocatorT = std::allocator<void>>
-  FilteredTransformListener(
+  TransformListener(
     tf2::BufferCore & buffer,
     NodeT && node,
     bool spin_thread = true,
-    const rclcpp::QoS & qos = tf2_ros::DynamicListenerQoS(),
-    const rclcpp::QoS & static_qos = tf2_ros::StaticListenerQoS(),
+    const rclcpp::QoS & qos = DynamicListenerQoS(),
+    const rclcpp::QoS & static_qos = StaticListenerQoS(),
     const rclcpp::SubscriptionOptionsWithAllocator<AllocatorT> & options =
-    tf2_ros::detail::get_default_transform_listener_sub_options<AllocatorT>(),
+    detail::get_default_transform_listener_sub_options<AllocatorT>(),
     const rclcpp::SubscriptionOptionsWithAllocator<AllocatorT> & static_options =
-    tf2_ros::detail::get_default_transform_listener_static_sub_options<AllocatorT>(),
-    bool static_only = false)
-  : FilteredTransformListener(
+    detail::get_default_transform_listener_static_sub_options<AllocatorT>())
+  : TransformListener(
+      buffer,
+      node->get_node_base_interface(),
+      node->get_node_logging_interface(),
+      node->get_node_parameters_interface(),
+      node->get_node_topics_interface(),
+      spin_thread,
+      qos,
+      static_qos,
+      options,
+      static_options)
+  {}
+
+  /** \brief Node constructor with static_only option */
+  template<class NodeT, class AllocatorT = std::allocator<void>>
+  TransformListener(
+    tf2::BufferCore & buffer,
+    NodeT && node,
+    bool spin_thread,
+    const rclcpp::QoS & qos,
+    const rclcpp::QoS & static_qos,
+    const rclcpp::SubscriptionOptionsWithAllocator<AllocatorT> & options,
+    const rclcpp::SubscriptionOptionsWithAllocator<AllocatorT> & static_options,
+    bool static_only)
+  : TransformListener(
       buffer,
       node->get_node_base_interface(),
       node->get_node_logging_interface(),
@@ -110,20 +157,47 @@ public:
 
   /** \brief Node interface constructor */
   template<class AllocatorT = std::allocator<void>>
-  FilteredTransformListener(
+  TransformListener(
     tf2::BufferCore & buffer,
     rclcpp::node_interfaces::NodeBaseInterface::SharedPtr node_base,
     rclcpp::node_interfaces::NodeLoggingInterface::SharedPtr node_logging,
     rclcpp::node_interfaces::NodeParametersInterface::SharedPtr node_parameters,
     rclcpp::node_interfaces::NodeTopicsInterface::SharedPtr node_topics,
     bool spin_thread = true,
-    const rclcpp::QoS & qos = tf2_ros::DynamicListenerQoS(),
-    const rclcpp::QoS & static_qos = tf2_ros::StaticListenerQoS(),
+    const rclcpp::QoS & qos = DynamicListenerQoS(),
+    const rclcpp::QoS & static_qos = StaticListenerQoS(),
     const rclcpp::SubscriptionOptionsWithAllocator<AllocatorT> & options =
-    tf2_ros::detail::get_default_transform_listener_sub_options<AllocatorT>(),
+    detail::get_default_transform_listener_sub_options<AllocatorT>(),
     const rclcpp::SubscriptionOptionsWithAllocator<AllocatorT> & static_options =
-    tf2_ros::detail::get_default_transform_listener_static_sub_options<AllocatorT>(),
-    bool static_only = false)
+    detail::get_default_transform_listener_static_sub_options<AllocatorT>())
+  : buffer_(buffer)
+  {
+    init(
+      node_base,
+      node_logging,
+      node_parameters,
+      node_topics,
+      spin_thread,
+      qos,
+      static_qos,
+      options,
+      static_options);
+  }
+
+  /** \brief Node interface constructor with static_only option */
+  template<class AllocatorT = std::allocator<void>>
+  TransformListener(
+    tf2::BufferCore & buffer,
+    rclcpp::node_interfaces::NodeBaseInterface::SharedPtr node_base,
+    rclcpp::node_interfaces::NodeLoggingInterface::SharedPtr node_logging,
+    rclcpp::node_interfaces::NodeParametersInterface::SharedPtr node_parameters,
+    rclcpp::node_interfaces::NodeTopicsInterface::SharedPtr node_topics,
+    bool spin_thread,
+    const rclcpp::QoS & qos,
+    const rclcpp::QoS & static_qos,
+    const rclcpp::SubscriptionOptionsWithAllocator<AllocatorT> & options,
+    const rclcpp::SubscriptionOptionsWithAllocator<AllocatorT> & static_options,
+    bool static_only)
   : buffer_(buffer)
   {
     init(
@@ -140,25 +214,11 @@ public:
   }
 
   TF2_ROS_PUBLIC
-  virtual ~FilteredTransformListener();
+  virtual ~TransformListener();
 
   /// Callback function for ros message subscription
   TF2_ROS_PUBLIC
   virtual void subscription_callback(tf2_msgs::msg::TFMessage::ConstSharedPtr msg, bool is_static);
-
-  // added filter rules vector
-  bool add_filter_rule(
-    std::function<bool(const geometry_msgs::msg::TransformStamped &)> _filter_rule)
-  {
-    filter_rules_.push_back(_filter_rule);
-    return true;
-  }
-
-  // added clear filter rules function
-  void clear_filter_rules()
-  {
-    filter_rules_.clear();
-  }
 
 private:
   template<class AllocatorT = std::allocator<void>>
@@ -171,8 +231,7 @@ private:
     const rclcpp::QoS & qos,
     const rclcpp::QoS & static_qos,
     const rclcpp::SubscriptionOptionsWithAllocator<AllocatorT> & options,
-    const rclcpp::SubscriptionOptionsWithAllocator<AllocatorT> & static_options,
-    bool static_only = false)
+    const rclcpp::SubscriptionOptionsWithAllocator<AllocatorT> & static_options)
   {
     spin_thread_ = spin_thread;
     node_base_interface_ = node_base;
@@ -180,27 +239,22 @@ private:
 
     using callback_t = std::function<void (tf2_msgs::msg::TFMessage::ConstSharedPtr)>;
     callback_t cb = std::bind(
-      &FilteredTransformListener::subscription_callback, this, std::placeholders::_1, false);
+      &TransformListener::subscription_callback, this, std::placeholders::_1, false);
     callback_t static_cb = std::bind(
-      &FilteredTransformListener::subscription_callback, this, std::placeholders::_1, true);
+      &TransformListener::subscription_callback, this, std::placeholders::_1, true);
 
     if (spin_thread_) {
       // Create new callback group for message_subscription of tf and tf_static
       callback_group_ = node_base_interface_->create_callback_group(
         rclcpp::CallbackGroupType::MutuallyExclusive, false);
-
-      if (!static_only) {
-        // Duplicate to modify subscription options
-        rclcpp::SubscriptionOptionsWithAllocator<AllocatorT> tf_options = options;
-        tf_options.callback_group = callback_group_;
-
-        message_subscription_tf_ = rclcpp::create_subscription<tf2_msgs::msg::TFMessage>(
-          node_parameters, node_topics, "/tf", qos, std::move(cb), tf_options);
-      }
-
+      // Duplicate to modify option of subscription
+      rclcpp::SubscriptionOptionsWithAllocator<AllocatorT> tf_options = options;
       rclcpp::SubscriptionOptionsWithAllocator<AllocatorT> tf_static_options = static_options;
+      tf_options.callback_group = callback_group_;
       tf_static_options.callback_group = callback_group_;
 
+      message_subscription_tf_ = rclcpp::create_subscription<tf2_msgs::msg::TFMessage>(
+        node_parameters, node_topics, "/tf", qos, std::move(cb), tf_options);
       message_subscription_tf_static_ = rclcpp::create_subscription<tf2_msgs::msg::TFMessage>(
         node_parameters,
         node_topics,
@@ -216,10 +270,73 @@ private:
       // Tell the buffer we have a dedicated thread to enable timeouts
       buffer_.setUsingDedicatedThread(true);
     } else {
-      if (!static_only) {
-        message_subscription_tf_ = rclcpp::create_subscription<tf2_msgs::msg::TFMessage>(
-          node_parameters, node_topics, "/tf", qos, std::move(cb), options);
-      }
+      message_subscription_tf_ = rclcpp::create_subscription<tf2_msgs::msg::TFMessage>(
+        node_parameters, node_topics, "/tf", qos, std::move(cb), options);
+      message_subscription_tf_static_ = rclcpp::create_subscription<tf2_msgs::msg::TFMessage>(
+        node_parameters,
+        node_topics,
+        "/tf_static",
+        static_qos,
+        std::move(static_cb),
+        static_options);
+    }
+  }
+
+  // Overload of init() with the static_only flag
+  template<class AllocatorT = std::allocator<void>>
+  void init(
+    rclcpp::node_interfaces::NodeBaseInterface::SharedPtr node_base,
+    rclcpp::node_interfaces::NodeLoggingInterface::SharedPtr node_logging,
+    rclcpp::node_interfaces::NodeParametersInterface::SharedPtr node_parameters,
+    rclcpp::node_interfaces::NodeTopicsInterface::SharedPtr node_topics,
+    bool spin_thread,
+    const rclcpp::QoS & qos,
+    const rclcpp::QoS & static_qos,
+    const rclcpp::SubscriptionOptionsWithAllocator<AllocatorT> & options,
+    const rclcpp::SubscriptionOptionsWithAllocator<AllocatorT> & static_options,
+    bool static_only)
+  {
+    if (!static_only) {
+      init(
+        node_base,
+        node_logging,
+        node_parameters,
+        node_topics,
+        spin_thread,
+        qos,
+        static_qos,
+        options,
+        static_options);
+      return;
+    }
+
+    spin_thread_ = spin_thread;
+    node_base_interface_ = node_base;
+    node_logging_interface_ = node_logging;
+
+    using callback_t = std::function<void (tf2_msgs::msg::TFMessage::ConstSharedPtr)>;
+    callback_t static_cb = std::bind(
+      &TransformListener::subscription_callback, this, std::placeholders::_1, true);
+
+    if (spin_thread_) {
+      callback_group_ = node_base_interface_->create_callback_group(
+        rclcpp::CallbackGroupType::MutuallyExclusive, false);
+      rclcpp::SubscriptionOptionsWithAllocator<AllocatorT> tf_static_options = static_options;
+      tf_static_options.callback_group = callback_group_;
+
+      message_subscription_tf_static_ = rclcpp::create_subscription<tf2_msgs::msg::TFMessage>(
+        node_parameters,
+        node_topics,
+        "/tf_static",
+        static_qos,
+        std::move(static_cb),
+        tf_static_options);
+
+      executor_ = std::make_shared<rclcpp::executors::SingleThreadedExecutor>();
+      executor_->add_callback_group(callback_group_, node_base_interface_);
+      dedicated_listener_thread_ = std::make_unique<std::thread>([&]() {executor_->spin();});
+      buffer_.setUsingDedicatedThread(true);
+    } else {
       message_subscription_tf_static_ = rclcpp::create_subscription<tf2_msgs::msg::TFMessage>(
         node_parameters,
         node_topics,
@@ -244,13 +361,79 @@ private:
   rclcpp::node_interfaces::NodeLoggingInterface::SharedPtr node_logging_interface_ {nullptr};
   rclcpp::node_interfaces::NodeBaseInterface::SharedPtr node_base_interface_ {nullptr};
   rclcpp::CallbackGroup::SharedPtr callback_group_{nullptr};
-
-
-  std::vector<std::function<bool(const geometry_msgs::msg::TransformStamped &)>> filter_rules_;
 };
 
-}  // namespace tf
+/** \brief Convenience class for subscribing to static-only transforms
+ *
+ * While users can achieve the same thing with a normal TransformListener, the number of parameters that must be
+ * provided is unwieldy.
+ */
+class StaticTransformListener : public TransformListener
+{
+public:
+  /** \brief Simplified constructor for a static transform listener
+   * \see the simplified TransformListener documentation
+   */
+  TF2_ROS_PUBLIC
+  explicit StaticTransformListener(tf2::BufferCore & buffer, bool spin_thread = true)
+  : TransformListener(buffer, spin_thread, true)
+  {
+  }
 
-}  // namespace as2
+  /** \brief Node constructor */
+  template<class NodeT, class AllocatorT = std::allocator<void>>
+  StaticTransformListener(
+    tf2::BufferCore & buffer,
+    NodeT && node,
+    bool spin_thread = true,
+    const rclcpp::QoS & static_qos = StaticListenerQoS(),
+    const rclcpp::SubscriptionOptionsWithAllocator<AllocatorT> & static_options =
+    detail::get_default_transform_listener_static_sub_options<AllocatorT>())
+  : TransformListener(
+      buffer,
+      node,
+      spin_thread,
+      rclcpp::QoS(1),
+      static_qos,
+      rclcpp::SubscriptionOptionsWithAllocator<AllocatorT>(),
+      static_options,
+      true)
+  {
+  }
 
-#endif  // AS2_CORE__UTILS__FILTERED_TRANSFORM_LISTENER_HPP_
+  /** \brief Node interface constructor */
+  template<class AllocatorT = std::allocator<void>>
+  StaticTransformListener(
+    tf2::BufferCore & buffer,
+    rclcpp::node_interfaces::NodeBaseInterface::SharedPtr node_base,
+    rclcpp::node_interfaces::NodeLoggingInterface::SharedPtr node_logging,
+    rclcpp::node_interfaces::NodeParametersInterface::SharedPtr node_parameters,
+    rclcpp::node_interfaces::NodeTopicsInterface::SharedPtr node_topics,
+    bool spin_thread = true,
+    const rclcpp::QoS & static_qos = StaticListenerQoS(),
+    const rclcpp::SubscriptionOptionsWithAllocator<AllocatorT> & static_options =
+    detail::get_default_transform_listener_static_sub_options<AllocatorT>())
+  : TransformListener(
+      buffer,
+      node_base,
+      node_logging,
+      node_parameters,
+      node_topics,
+      spin_thread,
+      rclcpp::QoS(1),
+      static_qos,
+      rclcpp::SubscriptionOptionsWithAllocator<AllocatorT>(),
+      static_options,
+      true)
+  {
+  }
+
+  TF2_ROS_PUBLIC
+  virtual ~StaticTransformListener()
+  {
+  }
+};
+
+}  // namespace tf2_ros
+
+#endif  // TF2_ROS__TRANSFORM_LISTENER_HPP_
--- as2_core/src/utils/filtered_transform_listener.cpp
+++ as2_core/src/utils/filtered_transform_listener.cpp
@@ -1,58 +1,45 @@
-// Copyright 2023 Universidad Politécnica de Madrid
-// Copyright (c) 2008, Willow Garage, Inc.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met:
-//
-//    * Redistributions of source code must retain the above copyright
-//      notice, this list of conditions and the following disclaimer.
-//
-//    * Redistributions in binary form must reproduce the above copyright
-//      notice, this list of conditions and the following disclaimer in the
-//      documentation and/or other materials provided with the distribution.
-//
-//    * Neither the name of the Universidad Politécnica de Madrid nor the names of its
-//      contributors may be used to endorse or promote products derived from
-//      this software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-// POSSIBILITY OF SUCH DAMAGE.
+/*
+ * Copyright (c) 2008, Willow Garage, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Willow Garage, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
-/*!*******************************************************************************************
- *  \file       filtered_transform_listener.cpp
- *  \brief      filtered_transform_listener implementation file.
- *  \authors    Tully Foote
- *              Miguel Fernandez Cortizas
- *              Rafael Perez Segui
- *              Rodrigo Da Silva Gómez
- ********************************************************************************/
+/** \author Tully Foote */
 
-
-#include "as2_core/utils/filtered_transform_listener.hpp"
 #include <memory>
 #include <string>
 #include <thread>
+#include <utility>
 
+#include "tf2_ros/transform_listener.hpp"
 
-namespace as2
+namespace tf2_ros
 {
 
-namespace tf
-{
-
-
-FilteredTransformListener::FilteredTransformListener(
-  tf2::BufferCore & buffer, bool spin_thread,
-  bool static_only)
+TransformListener::TransformListener(tf2::BufferCore & buffer, bool spin_thread)
 : buffer_(buffer)
 {
   rclcpp::NodeOptions options;
@@ -74,13 +61,40 @@ FilteredTransformListener::FilteredTransformListener(
     optional_default_node_->get_node_logging_interface(),
     optional_default_node_->get_node_parameters_interface(),
     optional_default_node_->get_node_topics_interface(),
-    spin_thread, tf2_ros::DynamicListenerQoS(), tf2_ros::StaticListenerQoS(),
-    tf2_ros::detail::get_default_transform_listener_sub_options(),
-    tf2_ros::detail::get_default_transform_listener_static_sub_options(),
+    spin_thread, DynamicListenerQoS(), StaticListenerQoS(),
+    detail::get_default_transform_listener_sub_options(),
+    detail::get_default_transform_listener_static_sub_options());
+}
+
+TransformListener::TransformListener(tf2::BufferCore & buffer, bool spin_thread, bool static_only)
+: buffer_(buffer)
+{
+  rclcpp::NodeOptions options;
+  // create a unique name for the node
+  // but specify its name in .arguments to override any __node passed on the command line.
+  // avoiding sstream because it's behavior can be overridden by external libraries.
+  // See this issue: https://github.com/ros2/geometry2/issues/540
+  char node_name[42];
+  snprintf(
+    node_name, sizeof(node_name), "transform_listener_impl_%zx",
+    reinterpret_cast<size_t>(this)
+  );
+  options.arguments({"--ros-args", "-r", "__node:=" + std::string(node_name)});
+  options.start_parameter_event_publisher(false);
+  options.start_parameter_services(false);
+  optional_default_node_ = rclcpp::Node::make_shared("_", options);
+  init(
+    optional_default_node_->get_node_base_interface(),
+    optional_default_node_->get_node_logging_interface(),
+    optional_default_node_->get_node_parameters_interface(),
+    optional_default_node_->get_node_topics_interface(),
+    spin_thread, DynamicListenerQoS(), StaticListenerQoS(),
+    detail::get_default_transform_listener_sub_options(),
+    detail::get_default_transform_listener_static_sub_options(),
     static_only);
 }
 
-FilteredTransformListener::~FilteredTransformListener()
+TransformListener::~TransformListener()
 {
   if (spin_thread_) {
     executor_->cancel();
@@ -88,7 +102,7 @@ FilteredTransformListener::~FilteredTransformListener()
   }
 }
 
-void FilteredTransformListener::subscription_callback(
+void TransformListener::subscription_callback(
   const tf2_msgs::msg::TFMessage::ConstSharedPtr msg,
   bool is_static)
 {
@@ -96,19 +110,6 @@ void FilteredTransformListener::subscription_callback(
   // TODO(tfoote) find a way to get the authority
   std::string authority = "Authority undetectable";
   for (size_t i = 0u; i < msg_in.transforms.size(); i++) {
-    // check the filter_rules_
-    // if any of the filter rules return false, skip this Transform
-    bool skip = false;
-    for (auto filter_rule : filter_rules_) {
-      if (!filter_rule(msg_in.transforms[i])) {
-        skip = true;
-        break;
-      }
-    }
-    if (skip) {
-      continue;
-    }
-
     try {
       buffer_.setTransform(msg_in.transforms[i], authority, is_static);
     } catch (const tf2::TransformException & ex) {
@@ -123,7 +124,4 @@ void FilteredTransformListener::subscription_callback(
   }
 }
 
-
-}  // namespace tf
-
-}  // namespace as2
+}  // namespace tf2_ros
